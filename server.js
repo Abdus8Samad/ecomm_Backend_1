e4d3290f508e91aeddb58bca5b9327f043ce92bb
Abdus8Samad <abdus8samad@gmail.com>
2020-07-04 20:20:23 +0530

    added user,categories middleware

--- server.js
+++ server.js
@@ -3,18 +3,48 @@ app = express(),
 bodyParser = require('body-parser'),
 PORT = process.env.PORT || 8080,
 mongoose = require('mongoose'),
+passport = require('passport'),
+expressSession = require('express-session'),
 morgan = require('morgan');
 require('dotenv/config');
 
 //Connect to the db
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
 .then(() => console.log('Connected to the DB'))
 .catch(err => console.log(err));
 
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:true}));
+//Express Session
+app.use(expressSession({
+    secret:'My secret!!',
+    resave:false,
+    saveUninitialized:false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
+//Models
+const Category = require('./models/category');
+
+//Middleware for All your routes
+app.use((req, res, next) => {
+    // assign each route the user object
+    res.locals.user = req.user;
+    next();
+});
+//MiddleWare for Category search
+app.use((req, res, next) =>{
+    // Look for All the Categories Available in the DB
+    Category.find()
+    .then(all =>{
+        res.locals.categories = all;
+    })
+    .catch(err =>{
+       return next(err);
+    })
+})
 //Routes
 const indexRoutes = require('./routes/index'),
 authRoutes = require('./routes/auth');
