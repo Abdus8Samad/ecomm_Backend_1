9fe01fbbdcc2b1ea9230af4e3c6998faaf7fe703
Abdus8Samad <abdus8samad@gmail.com>
2020-07-05 12:28:31 +0530

    Added localauth routes and fixed some err i fb,gp auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const router = require('express').Router(),
+User = require('../models/user'),
 passport = require('passport');
 
 //Login Page
@@ -6,13 +7,48 @@ router.get('/login',(req,res) =>{
     res.render('login');
 })
 
+//SignUp Page
+router.get('/register',(req,res) =>{
+    res.render('register');
+})
+
+//Register a new user or SignUp
+router.post('/register',(req,res) =>{
+    let newUser = {
+        email:req.body.email,
+        profile:{
+            username:req.body.username,
+            avatar:req.body.avatar
+        },
+        address:req.body.address
+    }
+    User.register(new User(newUser),req.body.password)
+    .then(user =>{
+        passport.authenticate("local")(req,res,() =>{
+            res.redirect('/profile');
+        })
+    })
+    .catch(err =>{
+        req.flash('error',err.message);
+        res.redirect('/register');
+    })
+})
+
+//Login post request to login the user
+router.post('/login',passport.authenticate('local',{
+    failureFlash:true,
+    failureRedirect:'/auth/login'
+}),(req,res) =>{
+    req.flash('success',`Successfully logged in, Welcome ${req.user.profile.username}`)
+    //Redirect to the profile page
+    res.redirect('/profile')
+})
+
 //Facebook auth route
 router.get('/facebook',passport.authenticate('facebook',{
     //Add more scopes if you want
     scope:['profile']
-}),(req,res) =>{
-    passport    
-})
+}))
 
 //Callback url for Facebook
 router.get('/facebook/redirect',passport.authenticate('facebook'),(req,res) =>{
@@ -25,9 +61,7 @@ router.get('/facebook/redirect',passport.authenticate('facebook'),(req,res) =>{
 router.get('/google',passport.authenticate('google',{
     //Add more scopes if you want
     scope:['profile']
-}),(req,res) =>{
-    passport    
-})
+}))
 
 //Callback url for google
 router.get('/google/redirect',passport.authenticate('google'),(req,res) =>{
@@ -39,6 +73,7 @@ router.get('/google/redirect',passport.authenticate('google'),(req,res) =>{
 //Auth Logout
 router.get('/logout',(req,res) =>{
     req.logOut();
+    //Redirect back to Home
     res.redirect('/');
 })
 
